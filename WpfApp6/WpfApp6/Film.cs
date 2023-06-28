dbc40540c1409607db2f7ed7e57f5b3a74888d39
mprzybylskixd <122105118+mprzybylskixd@users.noreply.github.com>
2023-06-28 14:24:16 +0200

    Add files via upload

---
+++ WpfApp6/WpfApp6/Film.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class Film : INotifyPropertyChanged
+    {
+        private string
+            title,
+            director,
+            publisher;
+        private DateTime?
+            publishDate = null;
+        public enum MediaType
+        {
+            VHS,
+            DVD,
+            BlueRay
+        }
+        private MediaType mediaType;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private static Dictionary<string, ICollection<string>> powiązaneWłaściwości
+        = new Dictionary<string, ICollection<string>>()
+        {
+            ["Title"] = new string[] { "title" },
+            ["Director"] = new string[] { "Director" },
+            ["Publisher"] = new string[] { "Publisher" },
+            ["MediaType"] = new string[] { "MediaType" },
+            ["PublishDate"] = new string[] { "Wiek" },
+            ["Wiek"] = new string[] { "SkrótSzczegółów" }
+        };
+
+        void NotyfikujZmianę(
+        [CallerMemberName] string? nazwaWłaściwości = null,
+        HashSet<string> jużZrobione = null
+        )
+        {
+            if (jużZrobione == null)
+                jużZrobione = new();
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(nazwaWłaściwości)
+                );
+            jużZrobione.Add(nazwaWłaściwości);
+            if (powiązaneWłaściwości.ContainsKey(nazwaWłaściwości))
+                foreach (string powiązanaWłaściwość in powiązaneWłaściwości[nazwaWłaściwości])
+                    if (jużZrobione.Contains(powiązanaWłaściwość) == false)
+                        NotyfikujZmianę(powiązanaWłaściwość, jużZrobione);
+        }
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                NotyfikujZmianę();
+            }
+        }
+        public string Director
+        {
+            get => director;
+            set
+            {
+                director = value;
+                NotyfikujZmianę();
+            }
+        }
+        public string Publisher
+        {
+            get => publisher;
+            set
+            {
+                publisher = value;
+                NotyfikujZmianę();
+            }
+        }
+        public DateTime? PublishDate
+        {
+            get => publishDate;
+            set
+            {
+                publishDate = value;
+                NotyfikujZmianę();
+            }
+        }
+        public ushort? Wiek
+        {
+            get
+            {
+                if (publishDate == null)
+                    return null;
+                DateTime? koniec = DateTime.Now;
+                TimeSpan różnica = (TimeSpan)(koniec - publishDate);
+                return (ushort)Math.Floor(różnica.Days / 365.25);
+            }
+        }
+        public string SkrótSzczegółów
+        {
+            get
+            {
+                if (publishDate == null)
+                    return title;
+                else
+                    return $"{title}, opublikowany {Wiek} lat temu";
+            }
+        }
+    }
+}
---
+++ WpfApp6/WpfApp6/ListaFilmów.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace WpfApp6;
+/// <summary>
+/// Logika interakcji dla klasy ListaOsób.xaml
+/// </summary>
+public partial class ListaFilmów : Window
+{
+    private const string ścieżkaIO = "listaFilmów.xml";
+    public ObservableCollection<Film> Filmy { get; set; } = new ObservableCollection<Film>();
+
+    ListBox lista;
+    public ListaFilmów()
+    {
+        DataContext = this;
+        InitializeComponent();
+        lista = (ListBox)this.FindName("Lista");
+    }
+
+    private void Edytuj(object sender, RoutedEventArgs e)
+    {
+        new WidokFilmu(
+            (Film)lista.SelectedItem
+            ).Show();
+    }
+
+    private void Dodaj(object sender, RoutedEventArgs e)
+    {
+        Film nowa = new Film();
+        Filmy.Add(nowa);
+        new WidokFilmu(
+            nowa
+            ).Show();
+    }
+
+    private void Usuń(object sender, RoutedEventArgs e)
+    {
+        Filmy.Remove(
+            (Film)lista.SelectedItem
+            );
+    }
+
+    private void Importuj(object sender, RoutedEventArgs e)
+    {
+        XmlSerializer serializator
+            = new XmlSerializer(typeof(ObservableCollection<Film>));
+        FileStream strumieńOdczytu = new FileStream(ścieżkaIO, FileMode.Open);
+        ObservableCollection<Film> filmy
+            = (ObservableCollection<Film>)serializator.Deserialize(
+                strumieńOdczytu
+                );
+        //Osoby = osoby; //nie działa, bo wiązanie jest do instancji
+        strumieńOdczytu.Close();
+        foreach (Film film in filmy)
+            Filmy.Add(film);
+    }
+
+    private void Eksportuj(object sender, RoutedEventArgs e)
+    {
+        XmlSerializer serializator
+            = new XmlSerializer(typeof(ObservableCollection<Film>));
+        TextWriter strumieńZapisu = new StreamWriter(ścieżkaIO);
+        serializator.Serialize(strumieńZapisu, Filmy);
+        strumieńZapisu.Close();
+    }
+}
